0e405a397ace266e0775b59e62d5a95e3596b070
Athlon007 <aathlon@outlook.com>
2021-04-27 20:15:23 +0200

    Updated sources

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-var source = 'https://api.github.com/repos/MSCLoaderPro/EarlyAccessRelease/releases';
+var source = 'https://api.github.com/repos/MSCLoaderPro/MSCModLoaderPro/releases';
+var sourceInstaller = 'https://api.github.com/repos/MSCLoaderPro/docs/releases';
 
 function success() {
     var data = JSON.parse(this.responseText);
-    document.getElementById("version").innerHTML = "Version: <b>" + data[0].tag_name + "</b>";
+    document.getElementById("version").innerHTML = "Version: <b>" + data[0].tag_name;
+}
+
+function success2() {
+    var data = JSON.parse(this.responseText);
+    document.getElementById("version2").innerHTML = "Installer: <b>" + data[0].tag_name;
 }
 
 function error(err) {
@@ -14,3 +20,9 @@ xhr.onload = success;
 xhr.onerror = error;
 xhr.open('GET', source);
 xhr.send();
+
+var xhr2 = new XMLHttpRequest();
+xhr2.onload = success2;
+xhr2.onerror = error;
+xhr2.open('GET', sourceInstaller);
+xhr2.send();
